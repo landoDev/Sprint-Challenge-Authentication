59b8e0e85955316c4b5dce1870b4b0b9d0755697
Landon Turner <lturner@teamworks.com>
2020-04-24 11:10:18 -0400

    implements register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); 
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body; // username, password
+
+  // rounds are 2 to the N times
+  const rounds = process.env.HASH_ROUNDS || 8;
+
+  // hash the user.password
+  const lockdown = bcrypt.hashSync(user.password, rounds)
+  // update the user to use the hash
+  user.password = lockdown;
+
+  Users.create(user).then(saved => {
+  res.status(201).json(saved)
+  }).catch(error => {
+  console.log(error);
+  res.status(500).json({ errorMessage: error.message })
+  })
 });
 
 router.post('/login', (req, res) => {
