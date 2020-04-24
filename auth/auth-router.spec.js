910ab4665ccf8325dd1834faf2b315a1b256d519
Landon Turner <lturner@teamworks.com>
2020-04-24 12:44:20 -0400

    adds minimum login tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -3,6 +3,10 @@ const server = require('../api/server');
 const db = require('../database/dbConfig')
 
 // maybe bring newUser object up here to be more DRY
+let newUser= {
+    username: 'Darth Vader',
+    password:'iamyourfather'
+};
 
 describe('auth-router', () => {
     describe('POST /register', () => {
@@ -10,16 +14,16 @@ describe('auth-router', () => {
             await db('users').truncate()
         })
         it('should return 201 on success', async () => {
-            const body = {username: 'Darth Vader', password:"iamyourfather" };
+            const body = newUser;
             const response = await request(server).post("/api/auth/register").send(body);
             expect(response.status).toEqual(201);
         });
 
         it('should add a user to the db', async function() {
-            const newUser= {
-                username: 'Darth Vader',
-                password:'iamyourfather'
-            };
+            // const newUser= {
+            //     username: 'Darth Vader',
+            //     password:'iamyourfather'
+            // };
             const existing = await db('users').where({username: newUser.username});
             expect(existing).toHaveLength(0)
             await request(server).post("/api/auth/register")
@@ -27,27 +31,28 @@ describe('auth-router', () => {
             const inserted = await db('users').where({username: newUser.username});
             expect(inserted).toHaveLength(1)
         });
-        it.todo('should recieve an object with a username and password property', () =>{
-            // use .toThrow when passing object with just username
-        })
+        it.todo('should reject an object without username or password properties'
+            // use .toThrow when passing object with just username and just password
+        )
     });
 
     describe("POST /login", () => {
         it('should return 200 on success', () => {
-            return request(server).post('/login')
-            .send({name: 'Darth Vader'})
+            return request(server).post('/api/auth/login')
+            .send(newUser)
             .then(res => {
-                expect(res.status).toBe(201);
+                expect(res.status).toBe(200);
             })
         });
 
         it('should return a welcome message to the user', async function() {
-            return request(server).post('/login')
-            .send({name: 'Darth Vader'})
+            return request(server).post('/api/auth/login')
+            .send(newUser)
             .then(res => {
-                expect(res.body.message).toBe("Not the proper message");
+                expect(res.body.message).toBe(`Welcome ${newUser.username}`);
             })
         });
+        it.todo('should return an unauthorized message when given the wrong password')
         it.todo('should produce a token')
     });
 
