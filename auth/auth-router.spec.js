6bf4b725c05d3c3971f5a51c5c694105ff31ff1a
Landon Turner <lturner@teamworks.com>
2020-04-24 12:27:07 -0400

    adds a passing test

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,56 @@
+const request = require('supertest')
+const server = require('../api/server');
+const db = require('../database/dbConfig')
+
+// maybe bring newUser object up here to be more DRY
+
+describe('auth-router', () => {
+    describe('POST /register', () => {
+        beforeEach(async () =>{
+            await db('users').truncate()
+        })
+        it('should return 201 on success', async () => {
+            const body = {username: 'Darth Vader', password:"iamyourfather" };
+            const response = await request(server).post("/api/auth/register").send(body);
+            expect(response.status).toEqual(201);
+        });
+
+        it('should add a user to the db', async function() {
+            const newUser= {
+                username: 'Darth Vader',
+                password:'iamyourfather'
+            };
+            const existing = await db('users').where({username: newUser.username});
+            expect(existing).toHaveLength(0)
+            const response = await request(server).post("api/auth/register")
+            .send({username: newUser.username})
+            .then(res => {
+                expect(res.body).toMatchObject({username: newUser.username});
+            })
+
+            const inserted = await db('users').where({username: newUser.username});
+            expect(inserted).toHaveLength(1)
+        });
+        it.todo('should recieve an object with a username and password property')
+    });
+
+    describe("POST /login", () => {
+        it('should return 200 on success', () => {
+            return request(server).post('/login')
+            .send({name: 'Darth Vader'})
+            .then(res => {
+                expect(res.status).toBe(201);
+            })
+        });
+
+        it('should return a welcome message to the user', async function() {
+            return request(server).post('/login')
+            .send({name: 'Darth Vader'})
+            .then(res => {
+                expect(res.body.message).toBe("Not the proper message");
+            })
+        });
+        it.todo('should produce a token')
+    });
+
+});
